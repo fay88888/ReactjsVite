7f9cdc1c293667e137863c42d1603648f6e383e3
fay88888 <poyay93@gmail.com>
2025-06-17 17:25:12 +0700

    login form add

--- compprops-react/src/App.jsx
+++ compprops-react/src/App.jsx
@@ -1,9 +1,14 @@
 import Greeting from "./components/Greeting";
-
+import Coba from "./components/finalproject";
+import ViewTable from "./components/boostraptable";
+import LoginFrom from "./components/LoginFrom";
 function App() {
   return (
     <div>
       <Greeting name="Hallo Fay" />
+      <Coba/>
+      <ViewTable/>
+      <LoginFrom/>
 
     </div>
   );
---
+++ compprops-react/src/components/finalproject.jsx
@@ -0,0 +1,10 @@
+function Coba() {
+  return (
+    <div className="container">
+      <h1 className="text-center">Hello, Bootstrap in React!</h1>
+    </div>
+  );
+}
+
+export default Coba;
+
